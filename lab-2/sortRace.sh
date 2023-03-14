7b095a754aaca3c3da702b4fc9553c0fb1a21752
Tuan M Tran <81743592+ttuan8600@users.noreply.github.com>
2023-03-14 11:40:41 -0700

    Update sortRace.sh

--- lab-2/sortRace.sh
+++ lab-2/sortRace.sh
@@ -2,3 +2,5 @@
 ./generate 1000000 100000 999999
 time sort numbers.txt > sysSort.out
 time ./mySort numbers.txt mySort.out
+time ./mySortA numbers.txt mySortA.out
+time ./mySortB numbers.txt mySortB.out
